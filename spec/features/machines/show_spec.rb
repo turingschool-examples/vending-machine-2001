ae7d2a9c6bd2b97a93c8b571d6a1d72bd4939e62
Jordan Sewell <jrsewell400@gmail.com>
2020-04-10 09:22:41 -0600

    Finish US1

--- spec/features/machines/show_spec.rb
+++ spec/features/machines/show_spec.rb
@@ -11,5 +11,23 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
   end
 
   it "I see the name of all of the snacks associated with that vending machine along with their price" do 
+    owner = Owner.create!(name: "Sam's Snacks")
+    dons  = owner.machines.create!(location: "Don's Mixed Drinks")
+    cranapple = Snack.create!(name: "Cranapple", price: "1.00")
+    crangrape = Snack.create!(name: "Crangrape", price: "1.50")
+    cranpineapple = Snack.create!(name: "Cranpineapple", price: "2.00")
+    MachineSnack.create!(machine: dons, snack: cranapple)
+    MachineSnack.create!(machine: dons, snack: crangrape)
+    MachineSnack.create!(machine: dons, snack: cranpineapple)
+
+    visit machine_path(dons)
+
+    expect(page).to have_content("Cranapple")
+    expect(page).to have_content("$1.00")
+    expect(page).to have_content("Crangrape")
+    expect(page).to have_content("1.50")
+    expect(page).to have_content("Cranpineapple")
+    expect(page).to have_content("2.00")
+    save_and_open_page
   end
 end
