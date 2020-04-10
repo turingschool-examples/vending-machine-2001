521c1fe34db08baeac570cfc2cb21a2e36be39c8
Maxwell Baird <bairdm71@gmail.com>
2020-04-10 09:40:33 -0600

    created the average test for snacks

--- spec/features/user_can_see_all_snacks_in_vending_spec.rb
+++ spec/features/user_can_see_all_snacks_in_vending_spec.rb
@@ -12,4 +12,17 @@ RSpec.describe "When a user visits the vending machine show page", type: :featur
     expect(page).to have_content("cheese sticks: $1.50")
     expect(page).to have_content("Chips n Dip: $3.00")
   end
+
+  scenario "they see a list of vending machine snacks" do
+    sam = Owner.create(name: "Sam's Snacks")
+    dons = sam.machines.create(location: "Don's Mixed Drinks")
+    dons.snacks.create(name: 'cheese sticks', price: 1.50)
+    dons.snacks.create(name: 'Chips n Dip', price: 3.50)
+    dons.snacks.create(name: "Hot Pockets", price: 2.50)
+
+    visit machine_path(dons)
+
+    expect(page).to have_content("Average price: $2.50")
+
+  end
 end
