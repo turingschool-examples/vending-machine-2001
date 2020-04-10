ff37abfbeb5bcdda35c11a1ba6449d2d7b209b36
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-10 10:10:47 -0600

    Update machine show page

--- spec/features/snack_show_spec.rb
+++ spec/features/snack_show_spec.rb
@@ -21,4 +21,25 @@ RSpec.describe "when I visit a snack show page" do
     expect(page).to have_content(snack1.name)
     expect(page).to have_content(snack1.price)
   end
+
+  it "I can also see a list of the locations with vending machines that have this snack" do
+    sam = Owner.create(name: "Sam's Snacks")
+    machine1 = sam.machines.create(location: "Don's Mixed Drinks")
+    machine2 = sam.machines.create(location: "Turing Basement")
+
+    snack1 = Snack.create(name: "White Castle Burger", price: 3.5)
+    snack2 = Snack.create(name: "Pop Rocks", price: 1.5)
+    snack3 = Snack.create(name: "Flaming Hot Cheetos", price: 2.5)
+
+    MachineSnack.create(machine_id: machine1.id, snack_id: snack1.id)
+    MachineSnack.create(machine_id: machine1.id, snack_id: snack1.id)
+    MachineSnack.create(machine_id: machine1.id, snack_id: snack2.id)
+    MachineSnack.create(machine_id: machine2.id, snack_id: snack2.id)
+
+    visit snack_path(snack2)
+
+    expect(page).to have_content("Locations:")
+    expect(page).to have_content(machine1.location)
+    expect(page).to have_content(machine2.location)
+  end
 end
