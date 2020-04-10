3be0e6df55339a7a0003ebf541c0c0f6e79c4722
Maxwell Baird <bairdm71@gmail.com>
2020-04-10 09:18:38 -0600

    created the test to see snack inside a vending machine

---
+++ spec/features/user_can_see_all_snacks_in_vending_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "When a user visits the vending machine show page", type: :feature do
+  scenario "they see a list of vending machine snacks" do
+    sam = Owner.create(name: "Sam's Snacks")
+    dons = sam.machines.create(location: "Don's Mixed Drinks")
+    dons.snacks.create(name: 'cheese sticks', price: 1.50)
+    dons.snacks.create(name: 'Chips n Dip', price: 3.00)
+
+    visit machine_path(dons)
+
+    expect(page).to have_content("cheese sticks: $1.50")
+    expect(page).to have_content("Chips n Dip: $3.00")
+  end
+end
