84ac4724937d4003b22aaae709003531ab066878
Javier Aguilar <jvr.agx@gmail.com>
2020-04-10 09:26:54 -0600

    Add machine show page that displays snacks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@owner = Owner.create(name: "Sam's Snacks")
+@dons  = @owner.machines.create(location: "Don's Mixed Drinks")
+
+@snack1 = Snack.create(name: "Hot Cheetos", price: 1.50)
+@snack2 = Snack.create(name: "Takis", price: 1.75)
+
+@dons.snacks << @snack1
+@dons.snacks << @snack2
---
+++ spec/features/machine/show_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor" do
+  describe "When I visit /machines/:id"
+    before do
+      @owner = Owner.create(name: "Sam's Snacks")
+      @dons  = @owner.machines.create(location: "Don's Mixed Drinks")
+      @snack1 = Snack.create(name: "Hot Cheetos", price: 1.50)
+      @snack2 = Snack.create(name: "Takis", price: 1.75)
+
+      @dons.snacks << @snack1
+      @dons.snacks << @snack2
+    end
+    it "I can see the name and prices of all snacks associated with that vending machine" do
+      visit "/machines/#{@dons.id}"
+
+      expect(page).to have_content(@snack1.name)
+      expect(page).to have_content(@snack1.price)
+      expect(page).to have_content(@snack2.name)
+      expect(page).to have_content(@snack2.name)
+
+    end
+end
