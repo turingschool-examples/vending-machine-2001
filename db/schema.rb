5c6f379bbdabefd6b8fc293bd71e967b86ca74c0
Anthony Gomes <code@tonygom.es>
2020-04-10 09:19:58 -0600

    Add snack table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914040805) do
+ActiveRecord::Schema.define(version: 20200410151457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20170914040805) do
 
   create_table "owners", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "snacks", force: :cascade do |t|
+    t.string "name"
+    t.float "price"
   end
 
   add_foreign_key "machines", "owners"
