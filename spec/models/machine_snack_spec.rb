36dcddc52ca68ec547935ceb0a84d7fc5db9af15
Javier Aguilar <jvr.agx@gmail.com>
2020-04-10 10:27:09 -0600

    Fix typos

--- spec/models/machine_snack_spec.rb
+++ spec/models/machine_snack_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe MachineSnack, type: :model do
-  describe "relationships" do
+  describe 'relationships' do
     it {should belong_to :machine}
     it {should belong_to :snack}
   end
