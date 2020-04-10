d3b720c6decc326a0bda3bafa6aa1f090cd36dd1
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-10 10:23:17 -0600

    Add average snack size method to machine model

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -4,4 +4,12 @@ class Machine < ApplicationRecord
   belongs_to :owner
   has_many :machine_snacks
   has_many :snacks, through: :machine_snacks
+
+  def average_snack_price
+    if snacks.size > 0
+      sum = snacks.sum { |snack| snack.price }
+      (sum/snacks.size).round(2)
+    end
+    # Snack.where(machine_id: id).average(:price)
+  end
 end
