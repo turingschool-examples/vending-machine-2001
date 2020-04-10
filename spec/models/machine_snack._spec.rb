fa39c3cd7b24bb241fd18f9d47d07410e123cf19
Kevin McGrevey <niv3kmcg@gmail.com>
2020-04-10 09:28:00 -0600

    Sets up databases and related model tests

---
+++ spec/models/machine_snack._spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe MachineSnack, type: :model do
+
+  describe 'relationships' do
+    it {should belong_to :machine}
+    it {should belong_to :snack}
+  end
+end
