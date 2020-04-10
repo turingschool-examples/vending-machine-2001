4ed4719508a2ee50eafa834bae84ffbaa288062b
Fred Rondina <fredrondina96@gmail.com>
2020-04-10 10:04:28 -0600

    Added model method test for average snack price

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe Machine, type: :model do
     it { should belong_to :owner }
     it { should have_many :snacks }
   end
+
+  describe "#average_snack_price" do
+    it "returns average snack price" do
+    sam = Owner.create!(name: "Sam's Snacks")
+    sam.machines.create!(location: "Don's Mixed Drinks")
+    sam.machines.first.snacks.create!(name: "Doritos", price: 2.99)
+    sam.machines.first.snacks.create!(name: "Cheetos", price: 1.99)
+    sam.machines.first.snacks.create!(name: "Fritos", price: 1.59)
+
+    expect(sam.machines.first.average_snack_price).to eq(2.19)
+  end
+  end
 end
