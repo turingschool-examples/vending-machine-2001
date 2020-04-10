91bee958d5185db9b7b523de9feed9e4f7105109
Jesse Gietzen <gietzenjesse@gmail.com>
2020-04-10 10:28:35 -0600

    out of time writing story 3 test

---
+++ spec/features/snacks/snacks_show_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "as a visitor, when i visit a snack show page", type: :feature do
+  "out of time, final commit!"
+end
+# User Story 3 of 3
+# As a visitor
+# When I visit a snack show page
+# I see the name of that snack
+#   and I see the price for that snack
+#   and I see a list of locations with vending machines that carry that snack
+#   and I see the average price for snacks in those vending machines
+#   and I see a count of the different kinds of items in that vending machine.
