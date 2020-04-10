dac537c52a7c0a55a96314169989990b67205f04
Alex Lynch <mlynch5187@gmail.com>
2020-04-10 09:19:38 -0600

    Create user story 1 test

---
+++ spec/features/machines/show_spec.rb.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'When a user visits a vending machine show page', type: :feature do
+  before(:each) do
+
+  @owner = Owner.create(name: "Sam's Snacks")
+  @dons  = @owner.machines.create(location: "Don's Mixed Drinks")
+  @gummies  = @owner.snacks.create(location: "Don's Mixed Drinks")
+  end
+
+  scenario 'they see the location of that machine' do
+
+    visit machine_path(dons)
+
+    expect(page).to have_content("Don's Mixed Drinks Vending Machine")
+  end
+
+  it "shows a list of snacks in the machine along with their price" do
+
+    visit machine_path(dons)
+
+    expect(page).to have_content("#{@gummies.name}")
+    expect(page).to have_content("#{@gummies.price}")
+  end
+
+end
+
+
+# As a visitor
+# When I visit a vending machine show page
+# I see the name of all of the snacks associated with that vending machine along with their price
