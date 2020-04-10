9c6aa17cc9ca7bca1f81210285b8089c66e90b40
Alex Lynch <mlynch5187@gmail.com>
2020-04-10 10:05:21 -0600

    Complete user story 2

--- spec/features/machines/show_spec.rb
+++ spec/features/machines/show_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
 
   @owner = Owner.create(name: "Sam's Snacks")
   @dons  = @owner.machines.create(location: "Don's Mixed Drinks")
-  @gummies  = @dons.snacks.create(name: "Gummies", price: 2)
+  @gummies  = @dons.snacks.create(name: "Haribo Gummies", price: 1)
+  @crab_chips  = @dons.snacks.create(name: "Utz Crab Chips", price: 3)
   end
 
   scenario 'they see the location of that machine' do
@@ -21,10 +22,19 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
 
     expect(page).to have_content("#{@gummies.name}")
     expect(page).to have_content("#{@gummies.price}")
+
+    expect(page).to have_content("#{@crab_chips.name}")
+    expect(page).to have_content("#{@crab_chips.price}")
+  end
+
+  it "shows average price of snacks" do
+
+    visit machine_path(@dons)
+
+    expect(page).to have_content("2")
   end
 end
 
-
 # As a visitor
 # When I visit a vending machine show page
-# I see the name of all of the snacks associated with that vending machine along with their price
+# I also see an average price for all of the snacks in that machine
