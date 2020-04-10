834b8d744d9106134f648c64266b230803a20730
Stephanie Friend <stephaniefriend91@gmail.com>
2020-04-10 10:24:57 -0600

    Add model testing with validation of

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -4,5 +4,27 @@ RSpec.describe Machine, type: :model do
   describe 'validations' do
     it { should validate_presence_of :location }
     it { should belong_to :owner }
+    it { should have_many :snacks }
+  end
+
+  describe 'instance methods' do
+    it 'should return average snack price' do
+      owner = Owner.create(name: "Sam's Snacks")
+      dons  = owner.machines.create(location: "Don's Mixed Drinks")
+      dons.snacks.create(name: "Potato Chips", price: 2)
+      dons.snacks.create(name: "Chocolate Bar", price: 4)
+      dons.snacks.create(name: "Pop Tart", price: 7)
+
+      expect(dons.average_snack_price).to eq(4.33)
+    end
+    it 'should return snack count per machine' do
+      owner = Owner.create(name: "Sam's Snacks")
+      dons  = owner.machines.create(location: "Don's Mixed Drinks")
+      dons.snacks.create(name: "Potato Chips", price: 2)
+      dons.snacks.create(name: "Chocolate Bar", price: 4)
+      dons.snacks.create(name: "Pop Tart", price: 7)
+
+      expect(dons.snack_count).to eq(3)
+    end
   end
 end
