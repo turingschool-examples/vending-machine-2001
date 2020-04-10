1e09fbb16934f40afe676716ab94888a46dd4d43
Margo Flewelling <flewelling.margo@gmail.com>
2020-04-10 09:40:49 -0600

    Write and pass tests for user story 1

--- spec/features/vending_machine_show_spec.rb
+++ spec/features/vending_machine_show_spec.rb
@@ -1,15 +1,36 @@
-# require 'rails_helper'
-#
-# RSpec.describe 'When a user visits a vending machine show page', type: :feature do
-#   scenario 'they see the location of that machine' do
-#     owner = Owner.create(name: "Sam's Snacks")
-#     dons  = owner.machines.create(location: "Don's Mixed Drinks")
-#
-#
-#     visit "/machines/#{dons.id}"
-#
-#
-#
-# As a visitor
-# When I visit a vending machine show page
-# I see the name of all of the snacks associated with that vending machine along with their price
+require 'rails_helper'
+
+RSpec.describe 'When a user visits a vending machine show page', type: :feature do
+  scenario 'they see the location of that machine' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    reeses = Snack.create(name: "Reeses", price: 1)
+    lara_bar = Snack.create(name: "Lara Bar", price: 2)
+    fig_newton = Snack.create(name: "Fig Newton", price: 2)
+    MachineSnack.create(machine_id: dons.id, snack_id: reeses.id)
+    MachineSnack.create(machine_id: dons.id, snack_id: lara_bar.id)
+
+    visit "/machines/#{dons.id}"
+
+    expect(page).to have_content(reeses.name)
+    expect(page).to have_content(reeses.price)
+    expect(page).to have_content(lara_bar.name)
+    expect(page).to have_content(lara_bar.price)
+    expect(page).to_not have_content(fig_newton.name)
+  end
+end
+
+
+As a visitor
+When I visit a vending machine show page
+I also see an average price for all of the snacks in that machine
+```
+
+### Example
+```Don's Mixed Drinks
+Snacks
+* White Castle Burger: $3.50
+* Pop Rocks: $1.50
+* Flaming Hot Cheetos: $2.50
+Average Price: $2.50
+``
