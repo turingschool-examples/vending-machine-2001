054423ea8ae995f68cb465306c4931a1f3f63e39
Anthony Gomes <code@tonygom.es>
2020-04-10 10:17:53 -0600

    Add average snack price method to machine model

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -4,4 +4,8 @@ class Machine < ApplicationRecord
   belongs_to :owner
   has_many :snack_machines
   has_many :snacks, through: :snack_machines
+
+  def average_snack_price
+    snacks.average(:price)
+  end
 end
