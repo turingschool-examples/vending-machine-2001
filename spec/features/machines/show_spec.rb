dbf970edc7775f2ae3d8271e71e844dd8acb72f8
Ryan Camp <campryan@comcast.net>
2020-04-10 10:08:59 -0600

    Complete user story 1

---
+++ spec/features/machines/show_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe "When I visit a machine show page" do
+  it "I see snack names and price in a list" do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    soda = dons.snacks.create(name: "soda", price: 3.50)
+    chips = dons.snacks.create(name: "chips", price: 1.50)
+    cheetos = dons.snacks.create(name: "cheetos", price: 2.50)
+
+    visit machine_path(dons)
+
+    within(".snack-list") do
+      expect(page).to have_content("#{soda.name}: $#{soda.price}")
+      expect(page).to have_content("#{chips.name}: $#{chips.price}")
+      expect(page).to have_content("#{cheetos.name}: $#{cheetos.price}")
+    end
+  end
+end
