fe99993c7c0e62816e0646ea6913d95c389758f3
Alex Latham <alexlthm@gmail.com>
2020-04-10 09:31:50 -0600

    Update model and features tests

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
+
+  scenario 'they see the name of all the snacks with that machine' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    white_castle = dons.snacks.create(name: "White Castle Burger", price: 3.5)
+    pop_rocks = dons.snacks.create(name: "Pop Rocks", price: 1.5)
+    hot_cheetos = dons.snacks.create(name: "Flaming Hot Cheetos", price: 2.5)
+
+    visit machine_path(dons)
+
+    expect(page).to have_content(white_castle.name)
+    expect(page).to have_content(pop_rocks.name)
+    expect(page).to have_content(hot_cheetos.name)
+  end
 end
