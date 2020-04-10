f2dcf1802b69adeb14ee11da205a0ca4945bf178
Stephanie Friend <stephaniefriend91@gmail.com>
2020-04-10 10:28:40 -0600

    Add snack machine model spec with more relationship testing

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Snack, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :price }
-    it { should have_many :machines }
+    it { should have_many :snack_machines }
+    it { should have_many(:machines).through(:snack_machines) }
   end
 end
