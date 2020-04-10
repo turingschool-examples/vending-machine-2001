d952434770193977202eff90015d3ae5b5128f4b
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-10 09:54:38 -0600

    Create model test and method finish vending machine view

--- spec/features/vending_machine_show_spec.rb
+++ spec/features/vending_machine_show_spec.rb
@@ -21,4 +21,22 @@ RSpec.describe "when I visit a machines show page", type: :feature do
     expect(page).to have_content("Flaming Hot Cheetos: $2.50")
 
   end
+
+  it "I also see the average price of snacks in the machine" do
+
+    sam = Owner.create(name: "Sam's Snacks")
+    machine = sam.machines.create(location: "Don's Mixed Drinks")
+
+    snack1 = Snack.create(name: "White Castle Burger", price: 3.5)
+    snack2 = Snack.create(name: "Pop Rocks", price: 1.5)
+    snack3 = Snack.create(name: "Flaming Hot Cheetos", price: 2.5)
+
+    MachineSnack.create(machine_id: machine.id, snack_id: snack1.id)
+    MachineSnack.create(machine_id: machine.id, snack_id: snack2.id)
+    MachineSnack.create(machine_id: machine.id, snack_id: snack3.id)
+
+    visit machine_path(machine)
+
+    expect(page).to have_content("Average Price: $2.00")
+  end
 end
--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -6,4 +6,22 @@ RSpec.describe Machine, type: :model do
     it { should belong_to :owner }
     it { should have_many :machine_snack }
   end
+
+  describe "instance methods" do
+    it "can average the price of its snacks" do
+
+      sam = Owner.create(name: "Sam's Snacks")
+      machine = sam.machines.create(location: "Don's Mixed Drinks")
+
+      snack1 = Snack.create(name: "White Castle Burger", price: 5)
+      snack2 = Snack.create(name: "Pop Rocks", price: 5)
+      snack3 = Snack.create(name: "Flaming Hot Cheetos", price: 2)
+
+      MachineSnack.create(machine_id: machine.id, snack_id: snack1.id)
+      MachineSnack.create(machine_id: machine.id, snack_id: snack2.id)
+      MachineSnack.create(machine_id: machine.id, snack_id: snack3.id)
+
+      expect(machine.average_snack_price).to eq(4)
+    end
+  end
 end
