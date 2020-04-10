2dbe496763d973cdee53c982d3493e03b92c12fe
Anthony Gomes <code@tonygom.es>
2020-04-10 09:30:57 -0600

    Update snack spec with relationship w/ machines through snack_machines

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe Snack, type: :model do
   describe "relationships" do
-    it { should have_many :snack_machines}
+    it { should have_many :snack_machines }
+    it { should have_many(:machines).through(:snack_machines) }
   end
 end
