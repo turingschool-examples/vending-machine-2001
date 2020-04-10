c651d383424068c92356096f0c118dbd76679499
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-04-10 09:42:57 -0600

    Complete User Story 2

--- spec/features/machines/show_spec.rb
+++ spec/features/machines/show_spec.rb
@@ -1,25 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "When a user visits the vending machine show", type: :feature do
+  before(:each) do
+    @sam = Owner.create!(name: "Sam's Snacks")
+    @drinks = @sam.machines.create!(location: "Don's Mixed Drinks")
+    @snack1 = Snack.create!(name: "Kool-Aid", price: 1)
+    @snack2 = Snack.create!(name: "Fruit Punch", price: 1.5)
+    @snack3 = Snack.create!(name: "Sweet Tea", price: 2)
+
+    SnackMachine.create!(snack_id: @snack1.id, machine_id: @drinks.id)
+    SnackMachine.create!(snack_id: @snack2.id, machine_id: @drinks.id)
+    SnackMachine.create!(snack_id: @snack3.id, machine_id: @drinks.id)
+  end
   scenario "they see the name of all of the snacks associated with that vending machine along with their price" do
-    sam = Owner.create!(name: "Sam's Snacks")
-    drinks = sam.machines.create!(location: "Don's Mixed Drinks")
-    snack1 = Snack.create!(name: "Kool-Aid", price: 1)
-    snack2 = Snack.create!(name: "Fruit Punch", price: 1.5)
-    snack3 = Snack.create!(name: "Sweet Tea", price: 2)
+    visit machine_path(@drinks)
 
-    SnackMachine.create!(snack_id: snack1.id, machine_id: drinks.id)
-    SnackMachine.create!(snack_id: snack2.id, machine_id: drinks.id)
-    SnackMachine.create!(snack_id: snack3.id, machine_id: drinks.id)
+    expect(current_path).to eq("/machines/#{@drinks.id}")
+    expect(page).to have_content(@snack1.name)
+    expect(page).to have_content(@snack1.price)
+    expect(page).to have_content(@snack2.name)
+    expect(page).to have_content(@snack2.price)
+    expect(page).to have_content(@snack3.name)
+    expect(page).to have_content(@snack3.price)
+  end
 
-    visit machine_path(drinks)
-
-    expect(current_path).to eq("/machines/#{drinks.id}")
-    expect(page).to have_content(snack1.name)
-    expect(page).to have_content(snack1.price)
-    expect(page).to have_content(snack2.name)
-    expect(page).to have_content(snack2.price)
-    expect(page).to have_content(snack3.name)
-    expect(page).to have_content(snack3.price)
+  scenario "they also wee an average price for all of the snacks in that machine" do
+    visit machine_path(@drinks)
+    expect(page).to have_content("Average Price of Snacks: $#{@drinks.average_snack_price}")
   end
 end
--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe Machine, type: :model do
     it { should validate_presence_of :location }
     it { should belong_to :owner }
   end
+
+  describe "#average_snack_price" do
+    it "can return average price of snacks that the machine has" do
+      @sam = Owner.create!(name: "Sam's Snacks")
+      @drinks = @sam.machines.create!(location: "Don's Mixed Drinks")
+      @snack1 = Snack.create!(name: "Kool-Aid", price: 1)
+      @snack2 = Snack.create!(name: "Fruit Punch", price: 1.5)
+      @snack3 = Snack.create!(name: "Sweet Tea", price: 2)
+
+      SnackMachine.create!(snack_id: @snack1.id, machine_id: @drinks.id)
+      SnackMachine.create!(snack_id: @snack2.id, machine_id: @drinks.id)
+      SnackMachine.create!(snack_id: @snack3.id, machine_id: @drinks.id)
+
+      expect(@drinks.average_snack_price).to eq(1.5)
+    end
+  end
 end
