5ec2dd42ff908ac202ea863abf17517be0f75210
Anthony Gomes <code@tonygom.es>
2020-04-10 10:01:42 -0600

    Update test with snack fields

--- spec/features/machines/user_can_see_vending_machine_snacks_listed_spec.rb
+++ spec/features/machines/user_can_see_vending_machine_snacks_listed_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
 
-    within("machine-snacks-main-block") do
+    within(".machine-snacks-main-block") do
       expect(page).to have_content(snack_1.name)
       expect(page).to have_content(snack_1.price)
 
