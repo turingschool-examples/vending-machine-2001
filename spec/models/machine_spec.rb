3dc46d34c59b710bf68cc5421e0b376b4305a5c1
Andrew Reid <areid.sj@gmail.com>
2020-04-10 10:08:48 -0600

    Add machine validation tests

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -19,6 +19,8 @@ RSpec.describe Machine, type: :model do
   describe 'validations' do
     it { should validate_presence_of :location }
     it { should belong_to :owner }
+    it {should have_many :machine_snacks }
+    it {should have_many :snacks }
   end
 
   describe 'methods' do
